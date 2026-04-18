c9960aae82d6cdc5bc6cef4b4185c387ee61cadb
HakoChest <zoneherobrine@gmail.com>
2026-04-18 15:30:06 +0800

    0.1.1 Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod core;
 pub mod create_config;
 pub mod discovery;
 pub mod error;
+#[cfg(feature = "cli")]
 pub mod interactive;
 pub mod mapper;
 
@@ -9,5 +10,6 @@ pub mod mapper;
 pub use core::{Config, KeyEvent, KeyboardMapper, MappingMode};
 pub use create_config::{create_config_interactive, select_local_config};
 pub use discovery::{display_configs, display_devices, list_configs, list_midi_devices};
+#[cfg(feature = "cli")]
 pub use interactive::{select_config, select_device, select_mode};
 pub use mapper::start_mapping;
